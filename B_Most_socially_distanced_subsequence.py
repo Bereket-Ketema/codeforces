278cf01a18c886c142f8db98ea712cf4c7d19042
Bereket-Ketema <bereketketema866@gmail.com>
2026-02-25 19:53:32 +0300

    most socially distanced subcequence problem have been solved

---
+++ B_Most_socially_distanced_subsequence.py
@@ -0,0 +1,17 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    result = [a[0]]
+
+    for i in range(1, n - 1):
+        
+        if (a[i] > a[i - 1] and a[i] > a[i + 1]) or \
+           (a[i] < a[i - 1] and a[i] < a[i + 1]):
+            result.append(a[i])
+
+    result.append(a[-1])
+
+    print(len(result))
+    print(*result)
